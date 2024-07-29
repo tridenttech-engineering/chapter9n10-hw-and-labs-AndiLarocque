66b0a2acf7f1f40b25ed61a3f5dae371091390c2
AndiLarocque <casandrelarocque@my.tridenttech.edu>
2024-07-29 18:29:39 -0400

    Update lab9-2.cpp

--- lab9-2.cpp
+++ lab9-2.cpp
@@ -6,42 +6,54 @@
 #include <iomanip>
 using namespace std;
 
+//function prototype
+double getPayment (int,double,int);
+
 int main()
 {
+int carPrice = 0;
+int rebate = 0; 
+double creditRate = 0.0;
+double dealerRate = 0.0;
+int term = 0; 
+double creditPayment = 0.0;
+double dealerPayment= 0.0; 
 
-int carPrice= 0;
-int rebate=0 ; 
-double creditRate= 0.0;
-double dealerRate=0;
-int term=0 
-double creditpayment=0.0;
-double dealerpayment=0.0; 
-
-    cout<<"Car price (after any trade in): ";
-    cin>>carPrice;
-    cout<< "Rebate:";
-    cin>>rebate;
-    cout<<"Credit union rate: ";
-    cin>>creditRate;
-    cout<<"Dealer rate: ";
-    cin>>dealerRate;
-    cout<<"Term in years: " ;
-    cin>> term;
+cout << "Car price (after any trade in): ";
+cin >> carPrice;
+cout << "Rebate: ";
+cin >> rebate;
+cout << "Credit union rate: ";
+cin >> creditRate;
+cout << "Dealer rate: ";
+cin >> dealerRate;
+cout << "Term in years: " ;
+cin >> term;
 
 //caculate payment
+    creditPayment= getPayment (carPrice - rebate, creditRate / 12, term * 12);
+    dealerPayment= getPayment (carPrice, dealerRate / 12, term * 12);
+
+
     
-    creditpayment= getPayemnt(carPrice - rebate,
-        creditRate/12, term * 12);
-    dealerPayment= getPayment(carPrice,dealerRate/ 12,
-        term * 12);
-
     //display payment
-
-    cout<<fixed<< setprecision(2)
-    cout<<"Credit union payment:  $"
-    <<creditPayment<<endl; 
-    cout<<"Dealer payment; $"
-        <<dealerPayment<<endl; 
-
+    cout << fixed << setprecision(2) << endl;
+    cout << "Credit union payment:  $" << creditPayment << endl; 
+    cout << "Dealer payment: $" << dealerPayment << endl; 
     return 0;
+    
 }//end of main function    
+
+//function definitions  
+double getPayment(int prin, double monthRate, int months)
+
+
+{
+    //caculates and returns monthly payments
+    
+    double monthPay= 0.0;
+    monthPay = prin * monthRate / (1-pow(monthRate + 1,-months));
+    
+    return monthPay;
+
+}    //end of getPayment function
