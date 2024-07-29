a7dafc0b03656a00616ebf31f143879b1aad777b
AndiLarocque <casandrelarocque@my.tridenttech.edu>
2024-07-29 17:37:46 -0400

    Update lab9-2.cpp

--- lab9-2.cpp
+++ lab9-2.cpp
@@ -9,5 +9,39 @@ using namespace std;
 int main()
 {
 
+int carPrice= 0;
+int rebate=0 ; 
+double creditRate= 0.0;
+double dealerRate=0;
+int term=0 
+double creditpayment=0.0;
+double dealerpayment=0.0; 
+
+    cout<<"Car price (after any trade in): ";
+    cin>>carPrice;
+    cout<< "Rebate:";
+    cin>>reabate;
+    cout<<"Credit union rate: ";
+    cin>>creditRate;
+    cout<<"Dealer rate: ";
+    cin>>dealerRate;
+    cout<<"Term in years: " ;
+    cin>> term;
+
+//caculate payment
+    
+    creditpayment= getPayemnt(carPrice - rebate,
+        creditRate/12, term * 12);
+    dealerPayment= getPayment(carPrice,dealerRate/ 12,
+        term * 12);
+
+    //display payment
+
+    cout<<fixed<< setprecision(2)
+    cout<<"Credit union payment:  $"
+    <<creditPayment<<endl; 
+    cout<<"Dealer payment; $"
+        <<dealerPayment<<endl; 
+
     return 0;
 }//end of main function    
